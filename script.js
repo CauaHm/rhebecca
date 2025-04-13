2a04ceb379ec943a7f595174f5e7abe617d9f02a
Cauã Henrique <144502865+CauaHm@users.noreply.github.com>
2025-04-13 12:08:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const messages = [
   "Eu te admiro demais",
   "Amo cada conversa, risada e assuntos aleatorios que você sempre me proporciona",
   "Em tão pouco tempo você se tornou uma pessoa muito especial pra mim",
-  "Nunca esquece o quanto você vale e da mulher incrivel que você é"
+  "Nunca se esqueça o quanto você vale e da mulher incrivel que você é"
 ];
 
 // Função que exibe a mensagem ao clicar na nuvem
@@ -15,7 +15,7 @@ function showMessage(message) {
   messageBox.innerHTML = message;
   
   // Simula a mensagem especial escondida
-  if (message === "Nunca esquece o quanto você vale e da mulher incrivel que você é") {
+  if (message === "Nunca se esqueça o quanto você vale e da mulher incrivel que você é") {
     setTimeout(() => {
       messageBox.innerHTML += "<br><br><strong>Mesmo longe, eu sempre vou querer te ver bem. Não importa o motivo ou qualquer razão que seja, pode contar cmg pra tudo! ❤️ obs: no momento dinheiro nao muito mas juro que quando estiver melhor eu tmb te ajudo KKKKKK </strong>";
       messageBox.style.backgroundColor = "#ff4081"; // Muda a cor para destaque
