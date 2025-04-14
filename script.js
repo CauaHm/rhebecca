38265658758629116d61bad0e8df46a329e25569
Cauã Henrique <144502865+CauaHm@users.noreply.github.com>
2025-04-13 23:40:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function showMessage(message) {
   // Simula a mensagem especial escondida
   if (message === "Nunca se esqueça o quanto você vale e da mulher incrivel que você é") {
     setTimeout(() => {
-      messageBox.innerHTML += "<br><br><strong>Mesmo longe, eu sempre vou querer te ver bem. Não importa o motivo ou qualquer razão que seja, pode contar cmg pra tudo! ❤️ obs: no momento dinheiro nao muito mas juro que quando estiver melhor eu tmb te ajudo KKKKKK </strong>";
+      messageBox.innerHTML += "<br><br><strong>Mesmo longe, biologicamente falando eu sempre vou querer te ver bem. Não importa o motivo ou qualquer razão que seja, pode contar cmg pra tudo! ❤️ obs: no momento dinheiro nao muito mas juro que quando estiver melhor eu tmb te ajudo KKKKKK </strong>";
       messageBox.style.backgroundColor = "#ff4081"; // Muda a cor para destaque
       messageBox.classList.add('special-message');
     }, 1000);
